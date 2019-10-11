13133ba97da293ea58f296d5c7593f23491e3060
kk0174lll <kk0174lll@gmail.com>
2019-10-11 14:21:04 +0500

    Count letters in string

---
+++ Count letters in string/main.py
@@ -0,0 +1,21 @@
+'''
+In this kata, you've to count lowercase letters in a given string and return the letter count in a hash with 'letter' as key and count as 'value'. The key must be 'symbol' instead of string in Ruby and 'char' instead of string in Crystal.
+
+Example:
+
+letter_count('arithmetics') #=> {"a": 1, "c": 1, "e": 1, "h": 1, "i": 2, "m": 1, "r": 1, "s": 1, "t": 2}
+'''
+
+
+def letter_count(s):
+  result = {}
+  for c in s:
+    if result.get(c,0) == 0:
+      result[c] = 1
+    else: 
+      result[c] = result[c] +1
+  return result
+def main():  
+  print(letter_count("codewars"))
+ 
+main()
